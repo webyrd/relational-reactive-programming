86f350c5f4352107bf7649c8d9f8353ffffa2f0b
William Byrd <webyrd@williamsmacbook.uconnect.utah.edu>
2016-01-05 15:06:34 -0700

    added simple temperature converter

---
+++ temperature.rkt
@@ -0,0 +1,81 @@
+#lang racket
+
+;;; Interactive temerature conversion, between F and C.
+;;; Inspired by Jonathan Edwards's Two-Way Dataflow example in Subtext: https://vimeo.com/106073134
+;;; Thanks to Matt Hammer for the suggestion!
+
+(require "mk.rkt")
+(require "numbers.rkt")
+(require racket/gui/base)
+
+;;; window size
+(define horiz 300)
+(define vert 300)
+
+(define horiz-center (quotient horiz 2))
+(define vert-center (quotient vert 2))
+
+
+
+(define (temp-conversion)
+  (let ((frame (new frame% (label "frame"))))
+    (let ((display-F (new message% (parent frame)
+                          (label "display-F")
+                          (auto-resize #t)))
+          (display-C (new message% (parent frame)
+                          (label "display-C")
+                          (auto-resize #t))))
+      
+      (letrec ((txt-F (new text-field%
+                           (label "F")
+                           (parent frame)
+                           (init-value "?")
+                           (callback (lambda (button event)
+                                       (let ((F-str (send button get-value)))
+                                         (let ((sp (open-input-string F-str)))
+                                           (let ((F (read sp)))
+                                             (let ((ans (run 1 (C)
+                                                          (fresh (C*9 C*9/5 rem)
+                                                            (pluso C*9/5 (build-num 32) F)
+                                                            (/o C*9 (build-num 5) C*9/5 rem)
+                                                            (*o C (build-num 9) C*9)))))
+                                               (if (null? ans)
+                                                   (begin
+                                                     (send display-C set-label "-")
+                                                     (send display-F set-label "-")
+                                                     (send txt-C set-value "?"))
+                                                   (begin
+                                                     (send display-F set-label (format "~s" F))
+                                                     (send display-C set-label (format "~s" (car ans)))
+                                                     (send txt-C set-value "?")))))))))))
+               (txt-C (new text-field%
+                           (label "C")
+                           (parent frame)
+                           (init-value "?")
+                           (callback (lambda (button event)
+                                       (let ((C-str (send button get-value)))
+                                         (let ((sp (open-input-string C-str)))
+                                           (display "reading C string...")
+                                           (newline)
+                                           (let ((C (read sp)))
+                                             (display "C: ")
+                                             (display C)
+                                             (newline)
+                                             (let ((ans (run 1 (F)
+                                                          (fresh (C*9 C*9/5 rem)
+                                                            (*o C (build-num 9) C*9)
+                                                            (/o C*9 (build-num 5) C*9/5 rem)
+                                                            (pluso C*9/5 (build-num 32) F)))))
+                                               (display "ans: ")
+                                               (display ans)
+                                               (newline)
+                                               (if (null? ans)
+                                                   (begin
+                                                     (send display-C set-label "-")
+                                                     (send display-F set-label "-")
+                                                     (send txt-F set-value "?"))
+                                                   (begin
+                                                     (send display-C set-label (format "~s" C))
+                                                     (send display-F set-label (format "~s" (car ans)))
+                                                     (send txt-F set-value "?"))))))))))))
+(send frame show #t)))))
